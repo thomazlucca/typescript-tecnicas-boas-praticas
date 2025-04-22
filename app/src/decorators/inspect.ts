58afded06df597c6032b28ad784c120a8346f197
Thomaz Lucca <thomazlucca@gmail.com>
2025-04-22 15:46:13 -0300

    adicionando outros decorators, escape e inspect, dom-injector

--- app/src/decorators/inspect.ts
+++ app/src/decorators/inspect.ts
@@ -1,5 +1,4 @@
-export function inspect() {
-    return function(
+export function inspect(
         target: any,
         propertyKey: string,
         descriptor: PropertyDescriptor
@@ -14,4 +13,3 @@ export function inspect() {
         }
         return descriptor;
     }
-}
